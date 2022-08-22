91e6839f617192454cf3b85e2e1b1b1117277e45
linfeng55020 <109973996+linfeng55020@users.noreply.github.com>
2022-08-22 21:26:42 +0800

    Add files via upload

---
+++ 代码第二天.cpp
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d\n", i);
+//	}
+//
+//	return 0;
+//}
+// for 循环建议前开后闭
+
+ 
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 10; i++)
+//	{
+//		for (; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//
+//
+//		}
+//
+//		return 0;
+//	}
+	//10个hehe 
+
+
+int main()
+{
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k = 1; k++)
+		k++;
+
+
+
+
+		return 0;
+
+}
